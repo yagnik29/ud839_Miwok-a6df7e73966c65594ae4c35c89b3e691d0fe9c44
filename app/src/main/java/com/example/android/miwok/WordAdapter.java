a276eab044a9dd050ddb535f8e4ba61716a86554
Yagnik Prajapati <yagnikprajapati@gmail.com>
2016-08-09 00:08:53 +0530

    constructor overloading feature

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -74,6 +75,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Return the whole list item layout  ( Containing 2 TextViews)
         // so that it can be shown in the ListView.
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
+        imageView.setImageResource(currentWord.getImageResourceId());
+
         return listItemView;
     }
 }
